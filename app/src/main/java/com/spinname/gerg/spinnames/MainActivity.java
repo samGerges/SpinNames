6606e3a8015ed10f6660ad1b405b9cd0d3ea1251
Samuel Gerges <samuel.w.gerges@gmail.com>
2018-07-01 06:31:12 -0400

    most functions are working now Date july 1st 2018

--- app/src/main/java/com/spinname/gerg/spinnames/MainActivity.java
+++ app/src/main/java/com/spinname/gerg/spinnames/MainActivity.java
@@ -1,11 +1,16 @@
 package com.spinname.gerg.spinnames;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.nfc.NfcAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,21 +19,46 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     private ArrayList<String> players = new ArrayList<String>();
-    private TextView nameField = findViewById(R.id.namesField);
-    private Button addButton =  findViewById(R.id.addButton);
 
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        Button addButton = findViewById(R.id.addButton);
+        Button playButton = findViewById(R.id.playButton);
+        Button clearButton = findViewById(R.id.clearButton);
+
+
+        final EditText namesField = findViewById(R.id.namesField);
+
+        playButton.setText("Pick Name");
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                players.add(namesField.getText().toString());
+                namesField.setText("");
+                initRecyclerView();
+            }
+        });
+
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                players.clear();
+                initRecyclerView();
+            }
+        });
     }
 
-    private void addClick(){
-        System.out.println("Hello");
+    private void initRecyclerView(){
+        RecyclerView recyclerView = findViewById(R.id.namesList);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(players, this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
-    private void shuffleNames(ArrayList<String> names){
 
-    }
 }
--- app/src/main/java/com/spinname/gerg/spinnames/RecyclerViewAdapter.java
+++ app/src/main/java/com/spinname/gerg/spinnames/RecyclerViewAdapter.java
@@ -1,33 +1,38 @@
 package com.spinname.gerg.spinnames;
 
+
+import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
 import java.util.ArrayList;
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>{
-    private ArrayList<String> names = new ArrayList<String>();
-    private static final String TAG = "RecyclerViewAdapter";
 
-    public RecyclerViewAdapter(ArrayList<String> names) {
+    private ArrayList<String> names = new ArrayList<String>();
+    private Context context;
+
+    public RecyclerViewAdapter(ArrayList<String> names, Context context) {
         this.names = names;
+        this.context = context;
     }
 
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.names_layout, parent, false);
         ViewHolder holder = new ViewHolder(view);
-        return null;
+        return holder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        Log.d(TAG, "onBindViewHolder: called");
-        holder.namesView.setText(names.get(position));
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.nameView.setText(names.get(position));
     }
 
     @Override
@@ -36,13 +41,15 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
-
-        TextView namesView;
+        TextView nameView;
+        ConstraintLayout parentLayout;
 
         public ViewHolder(View itemView) {
             super(itemView);
-            namesView = itemView.findViewById(R.id.nameView);
 
+            nameView = itemView.findViewById(R.id.nameView);
+            parentLayout = itemView.findViewById(R.id.parentlayout);
         }
     }
+
 }
